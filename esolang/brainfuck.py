6dd8493e19c65f0c14aeebcad2ac18e9fee98264
Maik Messerschmidt <maik.messerschmidt@gmx.net>
2018-10-29 17:34:46 +0100

    Added Brainfuck interpreter and some tests.

---
+++ esolang/brainfuck.py
@@ -0,0 +1,100 @@
+from collections import defaultdict
+
+import sys
+
+if sys.version_info.major < 3:
+    chr = unichr
+
+
+class Interpreter(object):
+    lang = "Brainfuck"
+    ext = ".b"
+
+    def __init__(self, cellsize=8):
+        """Create a new Brainfuck Interpreter.
+
+        Args:
+            cellsize: The size of a single memory cell in Bit.
+                      (can be set to None, which means infinite memory per cell.
+        """
+
+        if cellsize is None:
+            self.cellsize = cellsize
+            self.MAXNUM = float("inf")
+        else:
+            try:
+                self.cellsize = int(cellsize)
+                if cellsize < 1:
+                    raise ValueError()
+            except ValueError:
+                raise ValueError("cellsize must be int >= 1")
+
+            self.MAXNUM = 2 ** self.cellsize
+
+        self.memory = defaultdict(lambda: 0)
+        self.pointer = 0
+
+    def __build_jump_targets(self, code):
+        """Return a dict of jump targets."""
+        targets = {}
+        opening = []
+
+        for index, char in enumerate(code):
+            if char == "[":
+                opening.append(index)
+            elif char == "]":
+                try:
+                    open_idx = opening.pop()
+                    targets[open_idx] = index
+                    targets[index] = open_idx
+                except IndexError:
+                    raise ValueError("Bracket at char %d doesn't open." % index)
+
+        if opening:
+            raise ValueError("Bracket at char %d doesn't close." % opening.pop())
+
+        return targets
+
+    def run(self, code, infile=sys.stdin, outfile=sys.stdout):
+        jump_targets = self.__build_jump_targets(code)
+        index = 0
+
+        while True:
+            # Reached EOF.
+            if index >= len(code):
+                break
+
+            char = code[index]
+
+            if char == ">":
+                self.pointer += 1
+            elif char == "<":
+                self.pointer -= 1
+            elif char == "+":
+                self.memory[self.pointer] += 1
+                if self.cellsize:
+                    self.memory[self.pointer] %= self.MAXNUM
+            elif char == "-":
+                self.memory[self.pointer] -= 1
+                if self.cellsize:
+                    self.memory[self.pointer] %= self.MAXNUM
+            elif char == ".":
+                outfile.write(str(chr(self.memory[self.pointer])))
+            elif char == ",":
+                self.memory[self.pointer] = ord(infile.read())
+            elif char == "[":
+                if not self.memory[self.pointer]:
+                    index = jump_targets[index]
+            elif char == "]":
+                if self.memory[self.pointer]:
+                    index = jump_targets[index]
+
+            index += 1
+
+        return self.memory[self.pointer]
+
+    def memory_as_list(self):
+        """Return the internal memory as list."""
+        min_ptr = min(self.memory.keys())
+        max_ptr = max(self.memory.keys())
+        return [self.memory[ptr] for ptr in range(min_ptr, max_ptr+1)]
---
+++ esolang/tests/test_brainfuck.py
@@ -0,0 +1,76 @@
+"""Unittests for the Brainfuck interpreter.
+
+Code Examples taken from:
+ * https://esolangs.org/wiki/brainfuck
+"""
+
+from unittest import TestCase
+
+from io import StringIO
+
+from esolang.brainfuck import Interpreter
+
+HELLO_WORLD = """
+>++++++++[-<+++++++++>]<.>>+>-[+]++>++>+++[>[->+++<<+++>]<<]>-----.>->
+        +++..+++.>-.<<+[>[+>+]>>]<--------------.>>.+++.------.--------.>+.>+.
+"""
+
+HELLO_WORLD_OUTPUT = "Hello World!\n"
+
+HELLO_WORLD_2 = """
+++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.
++++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.
+"""
+
+HELLO_WORLD_2_OUTPUT = "Hello World!\n"
+
+CELLSIZE_TEST = """
+Calculate the value 256 and test if it's zero
+If the interpreter errors on overflow this is where it'll happen
+++++++++[>++++++++<-]>[<++++>-]
++<[>-<
+    Not zero so multiply by 256 again to get 65536
+    [>++++<-]>[<++++++++>-]<[>++++++++<-]
+    +>[>
+        # Print "32"
+        ++++++++++[>+++++<-]>+.-.[-]<
+    <[-]<->] <[>>
+        # Print "16"
+        +++++++[>+++++++<-]>.+++++.[-]<
+<<-]] >[>
+    # Print "8"
+    ++++++++[>+++++++<-]>.[-]<
+<-]<
+# Print " bit cells\n"
++++++++++++[>+++>+++++++++>+++++++++>+<<<<-]>-.>-.+++++++.+++++++++++.<.
+>>.++.+++++++..<-.>>-
+Clean up used cells.
+[[-]<]
+"""
+
+
+class BrainfuckTests(TestCase):
+    def run_code(self, code, interpreter=None):
+        """Run the brainfuck code and return the standard output as string."""
+        if interpreter is None:
+            interpreter = Interpreter()
+        outfile = StringIO()
+        interpreter.run(code, outfile=outfile)
+        outfile.seek(0)
+        return outfile.read()
+
+    def test_hello_world(self):
+        self.assertEqual(HELLO_WORLD_OUTPUT, self.run_code(HELLO_WORLD))
+
+    def test_hello_world_2(self):
+        self.assertEqual(HELLO_WORLD_2_OUTPUT, self.run_code(HELLO_WORLD_2))
+
+    def test_cellsize(self):
+        for cellsize in 8, 16, 32:
+            interpreter = Interpreter(cellsize=cellsize)
+            result = "%d bit cells\n" % cellsize
+            self.assertEqual(result, self.run_code(CELLSIZE_TEST, interpreter))
+
+        interpreter = Interpreter(cellsize=None)
+        result = "32 bit cells\n"
+        self.assertEqual(result, self.run_code(CELLSIZE_TEST, interpreter))
